a3d4ababfd2553627805574794abc10393e777f0
Ivan Psarev <psarew.ivan@yandex.ru>
2019-10-11 10:48:56 +0300

    Подключил компоненты bootstrap

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// Импортируем jQuery
+//= ../../node_modules/jquery/dist/jquery.js
+
+// Импортируем необходимые js-файлы Bootstrap 4
+//= ../../node_modules/bootstrap/js/dist/util.js
+//= ../../node_modules/bootstrap/js/dist/alert.js
+//= ../../node_modules/bootstrap/js/dist/button.js
+//= ../../node_modules/bootstrap/js/dist/collapse.js
